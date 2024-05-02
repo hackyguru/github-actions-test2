86197a9889cfdb2084c45c02addda2d85bad36b2
Guru <19eucs071@skcet.ac.in>
2024-05-03 01:39:10 +0530

    Update backup.js

--- backup.js
+++ backup.js
@@ -1,7 +1,7 @@
 const lighthouse = require('@lighthouse-web3/sdk');
 
 async function main() {
-  const apiKey = 7802fed8.6577b4f40c74435493704c05b06be2b1;
+  const apiKey = "7802fed8.6577b4f40c74435493704c05b06be2b1";
   const uploadResponse = await lighthouse.upload('./', apiKey);
 
   const cid = uploadResponse.data.Hash;
